50ae41eb79458b6c399492239033efefab1c3479
vanravid79-lab <vanravid79@gmail.com>
2026-06-02 16:02:31 +0700

    new

--- Floating Ads/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  var adContainer = document.getElementById('ad-container');
-  var clickCta = document.getElementById('clickCta');
-
-  clickCta.addEventListener('click', function() {
-    // This is the standard way to implement the clickTag
-    window.open("https://www.google.com/");
-  });
-};
